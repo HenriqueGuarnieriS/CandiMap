81f7b7f56249e7c3a4a8d48bc9b037c1004fdfd4
HenriqueGSG <henriqueguarnieri.gg@gmail.com>
2024-10-22 08:37:38 -0300

    update navbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,25 +5,22 @@ import useWindowWidth from "../utils/useWindowWidth";
 
 const renderMobile = () => {
   return (
-    <div className=" bg-yellow-400 flex   w-full ">
-      <nav className="  px-4 w-full ">
-        <ul className="flex  gap-4 w-full py-1  ">
-          <li className="">
-            <Link
-              className="z-50 font-semibold bg-white p-2 shadow rounded-md w-full flex flex-col items-center"
-              to="/"
-            >
-              <GiBrazil className="text-2xl" />
-              <span>Painel</span>
+    <div className=" bg-neutral-700 flex   w-full ">
+      <nav className="  px-4 py-2 ">
+        <ul className="flex  gap-4 w-full  justify-center items-center   ">
+          <li className="font-semibold  bg-neutral-800 text-yellow-500  px-3 py-2 shadow rounded-md w-full flex flex-col items-center">
+            <Link className="z-50 flex flex-col  items-center text-sm " to="/">
+              <MdSpaceDashboard className="w-7 h-7" />
+              Painel
             </Link>
           </li>
-          <li>
+          <li className="font-semibold bg-neutral-800 text-yellow-500 px-3 py-2 shadow rounded-md w-full flex flex-col items-center">
             <Link
-              className="z-50 font-semibold bg-white p-2 shadow rounded-md w-full flex flex-col items-center"
+              className="z-50 flex flex-col  items-center text-sm "
               to="/mapa"
             >
-              <MdSpaceDashboard className="text-2xl" />
-              <span>Mapa</span>
+              <GiBrazil className="w-7 h-7" />
+              Mapa
             </Link>
           </li>
         </ul>
